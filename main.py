dd0926a2991aceabbf305b3540e7f479ba7495e5
Kalindu-Karunarathna <kalindukarunarathne626@gmail.com>
2025-12-07 14:07:56 +0530

    create paddle class and add 2 paddles

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle,Screen
+from paddle import Paddle
 
 
 screen = Screen()
@@ -7,25 +8,35 @@ screen.bgcolor("black")
 screen.title("Pong Game")
 screen.tracer(0)
 
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5,stretch_len=1)
-paddle.penup()
-paddle.goto(350,0)
+# paddle = Turtle()
+# paddle.shape("square")
+# paddle.color("white")
+# paddle.shapesize(stretch_wid=5,stretch_len=1)
+# paddle.penup()
+# paddle.goto(350,0)
 
 
-def go_up():
-    new_y = paddle.ycor()+40
-    paddle.goto(350,new_y)
+# def go_up():
+#     new_y = paddle.ycor()+40
+#     paddle.goto(350,new_y)
+#
+# def go_down():
+#     new_y = paddle.ycor()-40
+#     paddle.goto(350,new_y)
 
-def go_down():
-    new_y = paddle.ycor()-40
-    paddle.goto(350,new_y)
+paddle_left = Paddle(-350)
+paddle_right = Paddle(350)
+
+
+
+
 
 screen.listen()
-screen.onkey(go_up,"Up")
-screen.onkey(go_down,"Down")
+screen.onkey(paddle_left.go_up,"w")
+screen.onkey(paddle_left.go_down,"s")
+screen.onkey(paddle_right.go_up,"Up")
+screen.onkey(paddle_right.go_down,"Down")
+
 
 
 is_game_on = True
